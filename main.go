cd4718babefc4403d3fbabb1d5c83e3beff7e489
haylieboscia <haylieboscia@gmail.com>
2024-04-10 20:21:24 -0700

    pretty print json to terminal

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -18,6 +19,17 @@ type Departments struct {
 	Name string `json:"displayname"`
 }
 
+// *notes: varname datatype
+
+func PrettyPrint(str []byte) (string, error) {
+	var prettyJson bytes.Buffer
+	if err := json.Indent(&prettyJson, str, "", "    "); err != nil {
+		return "", err
+	}
+	return prettyJson.String(), nil
+
+}
+
 func main() {
 
 	response, err := http.Get("https://collectionapi.metmuseum.org/public/collection/v1/departments")
@@ -31,14 +43,29 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Printing the return json
 	//fmt.Println(string(responseData))
 
+	// this is creating a new object called responseObject of type Response( struct above)
 	var responseObject Response
+
+	// by unmarshelling the JSON puts all the info into one block (ex: {dataWanted1 datawanted2})
 	json.Unmarshal(responseData, &responseObject)
 
-	for _, department := range responseObject.Departments {
+	// pretty prints out data
+	res, err := PrettyPrint(responseData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+
+	// data in orginal format
+	fmt.Println(responseObject)
+
+	//
+	/*for _, department := range responseObject.Departments {
 		fmt.Println("Department ID: ", department.ID)
 		fmt.Println("Department Name: ", department.Name)
 
-	}
+	}*/
 }
